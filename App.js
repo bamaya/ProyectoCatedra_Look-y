4e6cdff9bcd4e4bb070b0410088d948ed7c5ab9a
MoisesC16 <riukendo4000@gmail.com>
2022-10-13 20:52:30 -0600

    Se Agregar el Proyecto de Catedra App Look-y Primera Versión Base

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react from 'react';
+import { View} from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Bienvenida from './Components/bienvenida';
+import Registro from './Components/registro';
+import Login from './Components/login';
+import Home from './src/screens/home';
+import Navigation from './src/navigation/navigation';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Bienvenida" component={Bienvenida} options={{headerShown: false}} />
+        <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
+        <Stack.Screen name="Registro" component={Registro} options={{headerShown: false}} />
+        <Stack.Screen name="Root" component={Navigation} options={{headerShown: false}}  />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
