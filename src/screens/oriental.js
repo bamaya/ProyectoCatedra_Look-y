cc992f7f558a02864ef8f5421bee7105786e78f5
Byron Amaya <60157235+bamaya@users.noreply.github.com>
2022-10-16 14:49:13 -0600

    Update oriental.js

--- src/screens/oriental.js
+++ src/screens/oriental.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableHighlight, ScrollView, Image} from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity, TouchableHighlight, ScrollView, Image} from 'react-native';
 import * as Font from 'expo-font';
 
+
+  
 const hoteles = [
   {
     Nombre: 'Hotel 1',
@@ -18,23 +20,35 @@ const hoteles = [
   },
 ];
 
-export default function Oriental() {
+export default function Oriental(props) {
+  const NavigateToDecameron=props=>{
+    props.navigation.navigate('Decameron');
+  }
   return (
       <ScrollView style={styles.container}>
-        <Text style={styles.title1}>Zona horiental</Text>
+        <Text style={styles.title1}>Zona oriental</Text>
         {hoteles.map((u, i) => {
               return (
                 <React.Fragment key={i}>
                   <View>
                   <Text style={styles.subt}>{u.Nombre}</Text>
+                 
+                  <TouchableOpacity style={styles.ima} onPress={() => NavigateToDecameron(props) }>
+                  
                       <Image style={styles.ima} source={u.src} />
+                      
+          
+        </TouchableOpacity>
+                      
                   </View>
                 </React.Fragment>
               );
         })}
         <StatusBar style="auto" />
+        
       </ScrollView>
   );
+  
 }
 
 const styles = StyleSheet.create({
