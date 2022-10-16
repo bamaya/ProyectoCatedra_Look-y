c07ca2878c59b8267f6f74677a465caf39eae1f9
Byron Amaya <60157235+bamaya@users.noreply.github.com>
2022-10-16 14:47:43 -0600

    Update App.js

--- App.js
+++ App.js
@@ -7,17 +7,21 @@ import Registro from './Components/registro';
 import Login from './Components/login';
 import Home from './src/screens/home';
 import Navigation from './src/navigation/navigation';
+import Decameron from './src/screens/decameron';
+import SplashScreen from './src/screens/SplashScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName = "Splash">
+        <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Bienvenida" component={Bienvenida} options={{headerShown: false}} />
         <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
         <Stack.Screen name="Registro" component={Registro} options={{headerShown: false}} />
         <Stack.Screen name="Root" component={Navigation} options={{headerShown: false}}  />
+        <Stack.Screen name="Decameron" component={Decameron} options={{headerShown: false}}  />
       </Stack.Navigator>
     </NavigationContainer>
   );
