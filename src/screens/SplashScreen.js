37863279e7d828288bd243ba45e02876800483ee
Vladimir7w7 <vaasanchez2001@gmail.com>
2022-10-16 05:21:59 -0600

    Update 1.8v

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text,Image,Video } from "react-native";
 const SplashScreen = ({ navigation }) => {
     //Cuando pasen 3 segundos se navega a la pantalla Menu
     setTimeout(() => {
         //Elimina splash screen del stack
-        navigation.replace('Login');
+        navigation.replace('Bienvenida');
     }, 3000);
     return (
         <View style={styles.container}>
-            <Text >Splash Screen</Text>
+        <Image
+                        style={styles.bki2}
+                        source={require('../img/Logo_look-y.png')} />
+           <Image
+                        style={styles.bki}
+                        source={require('../img/load.gif')} />
+                        
           </View>
     )
 };
@@ -20,5 +26,16 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-    }
+    },
+    bki: {
+        
+        height:50,
+        width: 50,
+        marginTop:400
+      }, 
+      bki2: {
+        position: 'absolute',
+        height:860,
+        width: 400,
+      }, 
 });
